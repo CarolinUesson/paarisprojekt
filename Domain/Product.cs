dfb026f34e85379825ce2b2c97ae7f02f2e67fed
Liina Eneli Toiker <litoik@taltech.ee>
2024-04-30 20:49:42 +0300

    Lisatud klassid Domaini ja Facade´i

---
+++ Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Domain {
+    public sealed class Product(ProductData d): Entity<ProductData>(d) {
+        public string? Name => data.Name;
+    }
+}
