b9b833e4df1476eb5afc84c634ac6105eac40e37
Carolin Uesson <cauess@taltech.ee>
2024-05-09 16:19:19 +0300

    HtmlEditItem, -ShowItem loodud, Edit, Create, Delete, Details kasutavad neid

---
+++ Pages/HtmlEditItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Pages;
+public static class HtmlEditItem
+{
+    public static IHtmlContent EditItem<TModel, TValue>(this IHtmlHelper<TModel> h, Expression<Func<TModel, TValue>> e)
+    {
+        var label = h.LabelFor(e, new { @class = "control-label" } );
+        var editor = h.EditorFor(e, new { htmlAttributes = new { @class = "form-control" } } );
+        var validation = h.ValidationMessageFor(e, string.Empty, new { @class = "text-danger" });
+
+        var div = new TagBuilder("div");
+        div.AddCssClass("form-group");
+
+        div.InnerHtml.AppendHtml(label);
+        div.InnerHtml.AppendHtml(editor);
+        div.InnerHtml.AppendHtml(validation);
+
+        var writer = new StringWriter();
+        div.WriteTo(writer, HtmlEncoder.Default);
+
+        return new HtmlString(writer.ToString());
+    }
+}
---
+++ Pages/HtmlShowItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Pages;
+
+public static class HtmlShowItem
+{
+    public static IHtmlContent ShowItem<TModel, TValue>(this IHtmlHelper<TModel> h, Expression<Func<TModel, TValue>> e)
+    {
+        var label = h.DisplayNameFor(e);
+        var validation = h.DisplayFor(e);
+
+        var dt = new TagBuilder("dt");
+        dt.AddCssClass("col-sm-2");
+        dt.InnerHtml.AppendHtml(label);
+
+        var dd = new TagBuilder("dd");
+        dd.AddCssClass("col-sm-10");
+        dd.InnerHtml.AppendHtml(validation);
+
+        var writer = new StringWriter();
+        dt.WriteTo(writer, HtmlEncoder.Default);
+        dd.WriteTo(writer, HtmlEncoder.Default);
+
+        return new HtmlString(writer.ToString());
+    }
+}
