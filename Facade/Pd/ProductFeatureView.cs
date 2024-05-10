802c74897802fdb1831d9d5c03844606ba8f06a7
Carolin Uesson <cauess@taltech.ee>
2024-05-10 19:02:55 +0300

    EntityView on testitud

--- Facade/Pd/ProductFeatureView.cs
+++ Facade/Pd/ProductFeatureView.cs
@@ -5,7 +5,6 @@ namespace Facade.Pd
 {
     public sealed class ProductFeatureView : EntityView
     {
-        //[Required] public string Description { get; set; }
         [DisplayName("Description")][Required] public string? Description { get; set; }
     }
 }
---
+++ Tests.Facade/EntityViewTests.cs
@@ -0,0 +1,10 @@
+﻿using Facade;
+using Tests.Aids;
+
+namespace Tests.Facade;
+[TestClass] public class EntityViewTests : AbstractTests<EntityView, object>
+{
+    private class test : EntityView { }
+    protected override EntityView? createObject() => new test();
+    [TestMethod] public void IdTest() => propertyTest();
+}
