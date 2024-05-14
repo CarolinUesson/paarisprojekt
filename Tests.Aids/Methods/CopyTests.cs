26a63552320f92d1d042d747941b09e23ccd764d
Carolin Uesson <cauess@taltech.ee>
2024-05-14 18:14:13 +0300

    MembersTest

--- Tests.Aids/Methods/CopyTests.cs
+++ Tests.Aids/Methods/CopyTests.cs
@@ -9,9 +9,7 @@ public class CopyTests : BaseTests
     protected override Type? type => typeof(Copy);
     [TestMethod] public void MembersTest()
     {
-        var p = new ProductData();
-        p.Id = GetRnd.Int32();
-        p.Name = GetRnd.String();
+        var p = GetRnd.Object<ProductData>();
         var v = Copy.Members<ProductData, ProductView>(p);
         Assert.IsInstanceOfType(v, typeof(ProductView));
         Assert.AreEqual(p.Id, v.Id);
