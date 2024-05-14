448d3a5c55b604b5e36856484cafef7c9976f1bf
Carolin Uesson <cauess@taltech.ee>
2024-05-14 20:08:51 +0300

    Aids on testitud

--- Aids/Methods/GetSolution.cs
+++ Aids/Methods/GetSolution.cs
@@ -4,6 +4,7 @@ namespace Aids.Methods;
 public static class GetSolution
 {
     public static Assembly Assembly(string name) => System.Reflection.Assembly.Load(name);
-    public static List<Type> Types(string assemblyName) => 
-        Assembly(assemblyName).GetTypes().Where(x => !x.Name.StartsWith("<>")).ToList();
+    public static List<Type> Types(string assemblyName) => Types(Assembly(assemblyName));
+    public static List<Type> Types(Assembly a) => 
+        a.GetTypes().Where(x => !x.Name.StartsWith("<>")).ToList();
 }
---
+++ Tests.Aids/Methods/GetSolutionTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Aids.Methods;
+
+namespace Tests.Aids.Methods;
+[TestClass] public class GetSolutionTests : BaseTests
+{
+    protected override Type? type => typeof(GetSolution);
+    [TestMethod, DynamicData(nameof(testData), DynamicDataSourceType.Method)] public void AssemblyTest(string name)
+    {
+        var a = GetSolution.Assembly(name);
+        var e = Assembly.Load(name);
+        Assert.IsNotNull(e);
+        Assert.AreSame(e, a);
+    }
+    [TestMethod, DynamicData(nameof(testData), DynamicDataSourceType.Method)] public void TypesTest(string name)
+    {
+        var a = GetSolution.Types(name);
+        var e = GetSolution.Types(GetSolution.Assembly(name));
+        Assert.AreEqual(e.Count, a.Count);
+        foreach(var i in a) Assert.IsTrue(e.Contains(i));
+    }
+    [TestMethod, DynamicData(nameof(testData), DynamicDataSourceType.Method)] public void TypesByAssemblyTest(string name)
+    {
+        var assembly = Assembly.Load(name);
+        var a = GetSolution.Types(assembly);
+        var e = assembly.GetTypes().Where(x => !x.Name.StartsWith("<>")).ToList();
+        Assert.AreEqual(e.Count, a.Count);
+        foreach (var i in a) Assert.IsTrue(e.Contains(i));
+    }
+    private static IEnumerable<object[]> testData()
+    {
+        yield return new object[] { "Aids" };
+        yield return new object[] { "Tests.Aids" };
+    }
+}
