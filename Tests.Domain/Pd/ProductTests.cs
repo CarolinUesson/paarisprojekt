94fed2d020b6d12f75dd1d33de7510801599a08d
Carolin Uesson <cauess@taltech.ee>
2024-05-14 18:33:57 +0300

    Product on testitud

---
+++ Tests.Domain/Pd/ProductTests.cs
@@ -0,0 +1,17 @@
+﻿using Aids.Methods;
+using Data.Pd;
+using Domain;
+using Domain.Pd;
+using Tests.Aids;
+
+namespace Tests.Domain.Pd;
+[TestClass] public class ProductTests : SealedTests<Product, Entity<ProductData>>
+{
+    private ProductData data;
+    protected override Product? createObject()
+    {
+        data = GetRnd.Object<ProductData>();
+        return new Product(data);
+    }
+    [TestMethod] public void NameTest() => Assert.AreEqual(data?.Name, obj?.Name);
+}
