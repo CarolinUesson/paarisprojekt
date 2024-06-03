f357fb5a94048ae86992453b13ea6f2dfc230f03
Carolin Uesson <cauess@taltech.ee>
2024-06-03 13:39:08 +0300

    PriceComponent on testitud

---
+++ Tests.Domain/Pd/PriceComponentTests.cs
@@ -0,0 +1,11 @@
+﻿using Data.Pd;
+using Domain;
+using Domain.Pd;
+
+namespace Tests.Domain.Pd;
+[TestClass] public class PriceComponentTests : DomainClassTests<PriceComponent, PriceComponentData>
+{
+    protected override Type baseType => typeof(Date<PriceComponentData>);
+    [TestMethod] public void PriceTest() => valueTest(); 
+    [TestMethod] public void TypeTest() => valueTest();
+}
