e263258ffcb5eb0af212c9f0a7f2bd0b6cd54cba
Carolin Uesson <cauess@taltech.ee>
2024-06-03 13:09:50 +0300

    IPricedComponentsRepo on testitud

---
+++ Tests.Domain/Repos/IPriceComponentsRepoTests.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Common;
+using Domain.Pd;
+using Domain.Repos;
+
+namespace Tests.Domain.Repos;
+[TestClass] public class IPriceComponentsRepoTests : InterfaceTests<IPriceComponentsRepo, IPagedRepo<PriceComponent>>
+{
+}
