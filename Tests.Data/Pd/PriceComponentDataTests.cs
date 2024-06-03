2ce90c16aab1b4d4c7ccaceeb397af1b3af3639c
Carolin Uesson <cauess@taltech.ee>
2024-06-03 12:23:43 +0300

    PriceComponentData on testitud

---
+++ Tests.Data/Pd/PriceComponentDataTests.cs
@@ -0,0 +1,10 @@
+﻿using Tests.Aids;
+
+namespace Data.Pd;
+[TestClass] public class PriceComponentDataTests : SealedNewTests<PriceComponentData, EntityData>
+{
+    [TestMethod] public void FromDateTest() => propertyTest();
+    [TestMethod] public void ThruDateTest() => propertyTest();
+    [TestMethod] public void PriceTest() => propertyTest();
+    [TestMethod] public void TypeTest() => propertyTest();
+}
