a7b25ae52c0e221a08e0e090ea3ede44b4a7906f
Carolin Uesson <cauess@taltech.ee>
2024-06-03 12:44:43 +0300

    IDeploymentsRepo on testitud

---
+++ Tests.Domain/Repos/IDeploymentsRepoTests.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Common;
+using Domain.Pd;
+using Domain.Repos;
+
+namespace Tests.Domain.Repos;
+[TestClass] public class IDeploymentsRepoTests : InterfaceTests<IDeploymentsRepo, IPagedRepo<Deployment>>
+{
+}
