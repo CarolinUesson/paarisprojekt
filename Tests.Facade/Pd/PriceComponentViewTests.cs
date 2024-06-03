817d50aa4cedd38318feefdf3a8ef71af818ef80
Carolin Uesson <cauess@taltech.ee>
2024-06-03 13:56:19 +0300

    PriceComponentView on testitud

---
+++ Tests.Facade/Pd/PriceComponentViewTests.cs
@@ -0,0 +1,10 @@
+﻿using Facade.Pd;
+using Facade;
+using Tests.Aids;
+
+namespace Tests.Facade.Pd;
+[TestClass] public class PriceComponentViewTests : SealedNewTests<PriceComponentView, DateView>
+{
+    [TestMethod] public void PriceTest() => propertyTest("Price", true);
+    [TestMethod] public void TypeTest() => propertyTest("Type", true);
+}
