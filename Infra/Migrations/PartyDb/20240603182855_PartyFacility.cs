b9c78b63011ffe25bcc591b3332332bdb4af0b5c
Liina Eneli Toiker <litoik@taltech.ee>
2024-06-03 21:30:22 +0300

    Lisasin Party ja PartyFacility migratsioonid

---
+++ Infra/Migrations/PartyDb/20240603182855_PartyFacility.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations.PartyDb
+{
+    /// <inheritdoc />
+    public partial class PartyFacility : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
