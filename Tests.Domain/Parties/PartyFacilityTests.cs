10c35af5fe166f58f4673e4508dfe42761c05573
Liina Eneli Toiker <litoik@taltech.ee>
2024-06-03 21:50:30 +0300

    PartyFacility on testitud

---
+++ Tests.Domain/Parties/PartyFacilityTests.cs
@@ -0,0 +1,11 @@
+﻿using Data.Parties;
+using Data.Pd;
+using Domain;
+using Domain.Parties;
+using Tests.Domain.Pd;
+
+namespace Tests.Domain;
+[TestClass]
+public class PartyFacilityTests: DomainClassTests<PartyFacility, PartyFacilityData> {
+    [TestMethod] public void PartyIdTest() => valueTest();
+}
