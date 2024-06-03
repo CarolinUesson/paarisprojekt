1e9b9ec540948be829c83b9f67cad68d3164e212
Liina Eneli Toiker <litoik@taltech.ee>
2024-06-03 22:06:37 +0300

    IPartyRepo on testitud

---
+++ Tests.Domain/Repos/IPartyRepoTests.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Common;
+using Domain.Parties;
+using Domain.Repos.Parties;
+
+namespace Tests.Domain.Repos;
+[TestClass]
+public class IPartyRepoTests: InterfaceTests<IPartyRepo, IPagedRepo<Party>> {
+}
