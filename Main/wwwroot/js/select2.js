f1934c355d68a8054b8d77802fe6ac308febee04
Carolin Uesson <cauess@taltech.ee>
2024-06-04 17:59:11 +0300

    select2 kood täiendatud

--- Main/wwwroot/js/select2.js
+++ Main/wwwroot/js/select2.js
@@ -1,7 +1,32 @@
 ﻿$(".selectItems2").each(function () {
-    var $this = $(this);
+    var $this = $(this),
+        selectedId = $this.data('id'),
+        controller = $this.data('controller');
 
     $this.select2({
-        placeholder: "Select value",
+        placeholder: `Select value: controller=${controller}; id=${selectedId}`,
+        theme: "bootstrap4",
+        allowClear: true,
+        ajax: {
+            url: `/${controller}/selectItems`,
+            data: function (params) {
+                var query = {
+                    researchString: params.term,
+                    id: selectedId
+                };
+                return query;
+            },
+            processResults: function (result) {
+                return {
+                    results: $.map(result, function (item) {
+                        return {
+                            id: item.value,
+                            text: item.text,
+                            selected: item.selected
+                        };
+                    }),
+                };
+            }
+        }
     });
 });
