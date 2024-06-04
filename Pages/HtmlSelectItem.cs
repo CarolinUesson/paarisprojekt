4214bff2105c81f74187f8dfe5be991f7f0702da
Carolin Uesson <cauess@taltech.ee>
2024-06-04 17:17:17 +0300

    Teine SelectItem meetod IHtmlSelectItem klassis

--- Pages/HtmlSelectItem.cs
+++ Pages/HtmlSelectItem.cs
@@ -25,6 +25,26 @@ public static class HtmlSelectItem
 
         return new HtmlString(w.ToString());
     }
+    public static IHtmlContent SelectItem<TModel, TValue>(this IHtmlHelper<TModel> h, Expression<Func<TModel, TValue>> e, string controller)
+    {
+        var label = h.LabelFor(e, new { @class = "control-label" });
+        //var displayName = h.DisplayNameFor(e);
+        var n = h.NameFor(e);
+        var v = h.ValueFor(e);
+        var ed = new HtmlString($"<select name=\"{n}\" class=\"selectItems2 form-control\" data-controller=\"{controller}\" data-id=\"{v}\"></select>");
+        var validation = h.ValidationMessageFor(e, string.Empty, new { @class = "text-danger" });
+
+        var div = new TagBuilder("div");
+        div.AddCssClass("form-group");
+        div.InnerHtml.AppendHtml(label);
+        div.InnerHtml.AppendHtml(ed);
+        div.InnerHtml.AppendHtml(validation);
+
+        var w = new StringWriter();
+        div.WriteTo(w, HtmlEncoder.Default);
+
+        return new HtmlString(w.ToString());
+    }
 
     private static IEnumerable<SelectListItem> addDescr(SelectList sl, string displayName)
     {
