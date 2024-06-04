8698c3f11f27ca9bf9af17a0a422d61d28bb7fea
Liina Eneli Toiker <litoik@taltech.ee>
2024-06-04 13:58:14 +0300

    FacilityView testitud

---
+++ Tests.Facade/Parties/FacilityViewTests.cs
@@ -0,0 +1,9 @@
+﻿using Facade;
+using Facade.Parties;
+using Tests.Aids;
+
+namespace Tests.Facade.Pd;
+[TestClass]
+public class FacilityViewTests: SealedNewTests<FacilityView, EntityView> {
+    [TestMethod] public void LocationTest() => propertyTest("Location", true);
+}
