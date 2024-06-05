838020a91270d39826bd13a2dfc7e3bde0c510ef
Carolin Uesson <cauess@taltech.ee>
2024-06-05 13:59:53 +0300

    FacilityId on testitud

--- Tests.Facade/Parties/PartyFacilityViewTests.cs
+++ Tests.Facade/Parties/PartyFacilityViewTests.cs
@@ -6,4 +6,5 @@ namespace Tests.Facade.Pd;
 [TestClass]
 public class PartyFacilityViewTests: SealedNewTests<PartyFacilityView, EntityView> {
     [TestMethod] public void PartyIdTest() => propertyTest("Party", true);
+    [TestMethod] public void FacilityIdTest() => propertyTest("Facility", true);
 }
