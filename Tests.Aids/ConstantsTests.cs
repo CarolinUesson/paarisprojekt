e913767a7170d1aa1911e964272c313c1f346bfc
Carolin Uesson <cauess@taltech.ee>
2024-06-05 13:54:48 +0300

    Constants on testitud

---
+++ Tests.Aids/ConstantsTests.cs
@@ -0,0 +1,8 @@
+﻿using Aids;
+
+namespace Tests.Aids;
+[TestClass] public class ConstantsTests : BaseTests
+{
+    protected override Type? type => typeof(Constants);
+    [TestMethod] public void SelectTest() => Assert.AreEqual("Select", Constants.Select);
+}
